3f6da88273040712fcd3749ef47be479f0ea8a50
bryanMV <bryanmedina97@hotmail.com>
2020-11-11 14:40:48 -0500

    Enviar notificacion al usuario final

--- app/src/main/java/com/cyclenotification/Fcm.java
+++ app/src/main/java/com/cyclenotification/Fcm.java
@@ -1,8 +1,19 @@
 package com.cyclenotification;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+
+import com.e.periodizacionnatacion.MainActivity;
+import com.e.periodizacionnatacion.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -10,6 +21,9 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -50,18 +64,77 @@ public class Fcm extends FirebaseMessagingService {
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
 
-        String from = remoteMessage.getFrom();
-        Log.e("TAG","Mensaje de recibido" + from);
-
-        if(remoteMessage.getNotification() != null){
-            Log.e("TAG", "El título es:"+ remoteMessage.getNotification().getTitle());
-            Log.e("TAG", "El detalle es:"+ remoteMessage.getNotification().getBody());
-        }
-
         if(remoteMessage.getData().size() >0 ){
-            Log.e("TAG", "Título es:" + remoteMessage.getData().get("título"));
-            Log.e("TAG", "detalle es:" + remoteMessage.getData().get("detalle"));
-            Log.e("TAG", "el color es:" + remoteMessage.getData().get("título"));
+            String nombre = remoteMessage.getData().get("nombre");
+            String fecha = remoteMessage.getData().get("fecha");
+            String macro = remoteMessage.getData().get("macrociclo");
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                //Consultar la fecha antes de entrar al metodo
+                if (validarTiempo(fecha)){
+                    Log.e("Muestra", "El Nombre es:" + nombre);
+                    Log.e("Muestra", "La Fecha es:" + fecha);
+                    Log.e("Muestra", "El Macrociclo es:" + macro);
+                    mayorqueoreo(nombre,fecha,macro);
+                }else{
+                    Log.e("No muestra", "El Nombre es:" + nombre);
+                    Log.e("No muestra", "La Fecha es:" + fecha);
+                    Log.e("No muestra", "El Macrociclo es:" + macro);
+                }
+            }
         }
+    }
+
+    private void mayorqueoreo(String nombre, String fecha, String macro) {
+        String id = "Mensaje";
+
+        NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,id);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel nc = new NotificationChannel(id, "nuevo", NotificationManager.IMPORTANCE_HIGH);
+            nc.setShowBadge(true);
+            nm.createNotificationChannel(nc);
+        }
+
+        builder.setAutoCancel(true)
+                .setWhen(System.currentTimeMillis())
+                .setContentTitle("Macrociclo: "+macro)
+                .setSmallIcon(R.mipmap.logo_round)
+                .setContentText("Test: "+nombre+" / Fecha: "+fecha)
+                .setContentIntent(clicknotification())
+                .setContentInfo("nuevo");
+
+        Random rm = new Random();
+        int idNotify = rm.nextInt(8000);
+
+        assert nm != null;
+        nm.notify(idNotify,builder.build());
+
+    }
+
+    public PendingIntent clicknotification(){
+        Intent nf = new Intent(getApplicationContext(), MainActivity.class);
+        nf.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        return PendingIntent.getActivity(this,0,nf,0);
+    }
+
+    public boolean validarTiempo(String fecha){
+
+        boolean valido = false;
+
+        String[] notTest = fecha.split("-");
+
+        Calendar actual = Calendar.getInstance();
+        SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+        String[] act = df.format(actual.getTime()).split("-");
+
+        //Log.e("Actual Fcm",df.format(actual.getTime()));
+        if (Integer.parseInt(notTest[0]) == (Integer.parseInt(act[0]))
+                && Integer.parseInt(notTest[1]) == (Integer.parseInt(act[1]))
+                && Integer.parseInt(notTest[2]) == (Integer.parseInt(act[2]))){
+            valido = true;
+        }
+
+        return valido;
     }
 }
